bb180b2e0e42ebb407fc7d6683668960d883e4c4
liang <eric.chen@naturlabiokey.com>
2017-03-24 17:59:40 +0800

    20170324_1800

--- QS_ii/Program.cs
+++ QS_ii/Program.cs
@@ -3,11 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using LOGIN;
 namespace QS_ii
 {
-    static class Program
+    static class Program 
     {
+        private class UILOG : Login_main
+        {
+            
+            public override void Vlogin()
+            {
+
+                QS_ii_Quotes_add QiQ = new QS_ii_Quotes_add();
+                //QiQ.DMS_Service_ENV = Login_ServerCB.Text;       //server
+                //QiQ.DMS_UID = Login_ID_tb.Text;          //使用者ID
+                this.Hide();
+                QiQ.ShowDialog(this);
+                this.Close();
+            }
+
+        }
+        
         /// <summary>
         /// 應用程式的主要進入點。
         /// </summary>
@@ -16,7 +32,7 @@ namespace QS_ii
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new QS_ii_Quotes_add());
+            Application.Run(new UILOG());
         }
     }
 }
