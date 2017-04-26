26a715cb5d4aa41eb71683aee8f3b7fa17310986
liang <eric.chen@naturlabiokey.com>
2017-04-26 17:06:41 +0800

    20170426_1706

--- QS_ii/QS_ii_Quotes_add.cs
+++ QS_ii/QS_ii_Quotes_add.cs
@@ -895,11 +895,11 @@ namespace QS_ii
             QS_iiQ_add.Product_Query(QS_ii_QueryDGv_PID, QS_iiQ_add.QSiiDB.QS_ii_Product, QS_ii_DGView1);        //商品主檔查詢
             
         }       
-
         
-        public override void QS_ii_QueryDGV_加入button()
-        {   
-            
+        public override void QS_ii_QueryDGV_加入button()      //報價單明細檔多選的加入button
+        {
+            QS_iiQ_add.QS_ii_PriceDataBinding(QS_iiQ_add.QSiiDB.QS_ii_QProduct);           //datatable的欄位與Text綁定資料
+            QS_iiQ_add.DataTable_SETColumnExpression();                 //設定DataTable的Column.Expression
             DataView QS_ii_DView = new DataView(QS_iiQ_add.QSiiDB.QS_ii_Product);
             DataView QS_ii_QDView = new DataView(QS_iiQ_add.QSiiDB.QS_ii_QProduct);
             QS_ii_DView.RowFilter = "Check = '1'";
@@ -916,6 +916,7 @@ namespace QS_ii
                 QS_ii_dr["UNIT_PRICE"] = DView["單價"];
                 QS_ii_dr.EndEdit();                      
             }
+            this.Close();
             
             //foreach (DataRowView DView in QS_ii_DView)
             //{
